522d69ba2e67484c9a177c69fc03d22f2261f0bc
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-03-21 20:35:53 +0500

    fix: update close button hover color for improved accessibility in contact modal

--- src/pages/services/detailed-service.php
+++ src/pages/services/detailed-service.php
@@ -131,7 +131,7 @@
                 <!-- Modal Header -->
                 <div class="relative p-6 pb-3 flex justify-between items-center">
                     <h2 class="text-2xl font-semibold">Contact with email</h2>
-                    <button id="closeModal" class="text-gray-400 hover:text-white focus:outline-none">
+                    <button id="closeModal" class="text-gray-400 hover:text-gray-700 dark:hover:text-white focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
